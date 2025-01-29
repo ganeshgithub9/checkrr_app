0ff5893029a770d25f03ee02f709c2359173d2d5
ganesh-busam <ganesh.busam@zemosolabs.com>
2025-01-29 12:40:31 +0530

    chore: project initial structure

---
+++ src/main/java/com/example/checkrr/CheckrrApplication.java
@@ -0,0 +1,13 @@
+package com.example.checkrr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckrrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckrrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/checkrr/CheckrrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.checkrr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckrrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
